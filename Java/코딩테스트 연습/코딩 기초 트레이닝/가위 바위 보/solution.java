d7c0b0ea10e9ea14656c9ca9d2dcd1eb3b1888b4
dlanaraa <immmnara@gmail.com>
2025-11-27 01:04:20 +0900

    solve: 가위 바위 보

---
+++ Java/코딩테스트 연습/코딩 기초 트레이닝/가위 바위 보/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            if (rsp.charAt(i) == '2') {
+                answer.append("0");
+            }
+            else if (rsp.charAt(i) == '0') {
+                answer.append("5");
+            }
+            else {
+                answer.append("2");
+            }
+        }
+        return answer.toString();
+    }
+}
