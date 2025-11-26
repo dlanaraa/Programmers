95fcdf52a1e464895dcd7ce52860f959cc554593
dlanaraa <immmnara@gmail.com>
2025-11-27 02:02:20 +0900

    solve: 대문자와 소문자

---
+++ Java/코딩테스트 연습/코딩 기초 트레이닝/대문자와 소문자/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else {
+                result.append(Character.toUpperCase(c));
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+
+// class Solution {
+//     public String solution(String my_string) {
+//         StringBuilder result = new StringBuilder();
+        
+//         for (int i = 0; i < my_string.length(); i++) {
+//             char c = my_string.charAt(i);
+//             result.append(Character.isUpperCase(c) ? 
+//                           Character.toLowerCase(c) :
+//                           Character.toUpperCase(c));
+//         }
+
+//         return result.toString();
+//     }
+// }
