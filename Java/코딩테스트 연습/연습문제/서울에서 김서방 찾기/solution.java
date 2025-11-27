ada4d5e80d87971af07bac8394e85aa61774af4d
dlanaraa <immmnara@gmail.com>
2025-11-27 23:53:30 +0900

    solve: 서울에서 김서방 찾기

---
+++ Java/코딩테스트 연습/연습문제/서울에서 김서방 찾기/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int idx = 0;
+        
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                idx = i;
+                break;
+            } 
+        }
+        
+        return "김서방은 " + idx + "에 있다";
+    }
+}
+
+
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String[] seoul) {
+//         int idx = Arrays.asList(seoul).indexOf("Kim");
+//         return "김서방은 " + idx + "에 있다";
+//     }
+// }
