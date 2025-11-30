5432ebe36210f7223df619d06ac6466e62cccac7
dlanaraa <immmnara@gmail.com>
2025-11-30 10:50:56 +0900

    solve: 두 개 뽑아서 더하기

---
+++ Java/코딩테스트 연습/월간 코드 챌린지 시즌1/두 개 뽑아서 더하기/solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);  
+            }
+        }
+
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for (int v : set) answer[idx++] = v;
+
+        Arrays.sort(answer);
+        return answer;
+    }
+}
+
+// import java.util.HashSet;
+// import java.util.Set;
+
+// class Solution {
+//      public int[] solution(int[] numbers) {
+//         Set<Integer> set = new HashSet<>();
+
+//         for(int i=0; i<numbers.length; i++) {
+//             for(int j=i+1; j<numbers.length; j++) {
+//                 set.add(numbers[i] + numbers[j]);
+//             }
+//         }
+
+//         return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+//     }
+// }
